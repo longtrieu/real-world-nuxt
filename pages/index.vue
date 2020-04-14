f6826156a697b00650288d4e8c6df37ae9dd9bcf
Long Trieu <longtrieu@okiela.com>
2020-04-14 15:45:40 +0700

    Add nuxt/axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <div><h1>Events</h1></div>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then((response) => {
+        return {
+          events: response.data
+        }
+      })
+      .catch((e) => {
+        error({
+          status: 503,
+          message: 'Unable to fetch events at this time. Please try again later'
+        })
+      })
+  },
   head() {
     return {
       title: 'Event Listing'
